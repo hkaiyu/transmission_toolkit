f949c48e68a61ec13883b0deb5b1a1413cec9913
Kaiyu Hernandez <kaiyuhernandez@gmail.com>
2020-06-23 16:18:48 -0500

    edited tree visualization

---
+++ transmission-kaiyu/visualize_tree.py
@@ -0,0 +1,13 @@
+import ipyrad.analysis as ipa
+import toytree
+
+def visualize_tree(path_to_file):
+    """
+    """
+    rax = ipa.raxml(data=path_to_file, T=4, N=10)
+    rax.run(block=True, force=True)
+    tree = toytree.tree(rax.trees.bipartitions)
+    draw = tree.root(wildcard="prz")
+    draw.draw(tip_labels_align=True, node_labels="support")
+
+visualize_tree(r"\Users\sackb\Documents\COVID-19\parnsp.mfa")
