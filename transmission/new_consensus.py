bf410e3c852f755904ce2d85c78608f083412c66
marybrady722 <64038527+marybrady722@users.noreply.github.com>
2020-07-04 14:29:01 -0400

    Update new_consensus.py

--- transmission/new_consensus.py
+++ transmission/new_consensus.py
@@ -1,66 +1,24 @@
-def consensus_variants(vcf_file):
+import vcf
+from transmission_toolkit import parsers
+
+def consensus_sequence(vcf_file, reference_sequence):
     """
-    Inputs:
-        vcf_file: a vcf file
-    Outputs:
-        variants: most common variant that replaces reference in consensus, in the form (position, variant)
     """
-    vcf_read = vcf.Reader(open(vcf_file, 'r'))
-    variants = []
-    previous = (0, 0, 0) #(position, variant, allele frequency)
-    previous_previous = (0, 0, 0) #(position, variant, allele frequency)
-    for record in vcf_read:
-        if record.INFO['AF'] > 0.5: #keeps reference if exactly 0.5
-            variants.append((record.POS, record.ALT))
-        #2 variants
-        if record.POS == previous[0] and record.POS != previous_previous[0]:
-            two_ref = (1 - record.POS - previous[2])
-            if previous[2] > 0.5:
-                previous_previous = previous
-                previous = (record.POS, record.ALT, record.INFO['AF'])
-                break
-            elif two_ref > record.POS and two_ref > previous[0]:
-                previous_previous = previous
-                previous = (record.POS, record.ALT, record.INFO['AF'])
-                break
-            elif record.POS > two_ref and record.POS > previous[2]:
-                variants.append((record.POS, record.ALT))
-            elif previous[2] > two_ref and previous[2] > record.POS:
-                variants.append((previous[0], previous[1]))
+    variants = parsers.extract_lfv(vcf_file)
+    highest_variants = {}
 
-        #3 variants
-        if record.POS == previous[0] and record.POS == previous_previous[0]:
-            three_ref = (1 - record.POS - previous[2] - previous_previous[2])
-            if previous[2] > 0.5 or previous_previous[2] > 0.5:
-                previous_previous = previous
-                previous = (record.POS, record.ALT, record.INFO['AF'])
-                break
-            elif three_ref > record.POS and three_ref > previous[2] and three_ref > previous_previous[2]:
-                #ref allele is greatest freq
-                previous_previous = previous
-                previous = (record.POS, record.ALT, record.INFO['AF'])
-                break
-            elif record.POS > three_ref and record.POS > previous[2] and record.POS > previous_previous[2]:
-                variants.append((record.POS, record.ALT))
-            elif previous[2] > two_ref and previous[2] > record.POS and previous[2] > previous_previous[2]:
-                variants.append(previous[0], previous[1])
-            elif previous_previous[2] > two_ref and previous_previous[2] > record.POS and previous_previous[2] > previous[2]:
-                variants.append(previous_previous[0], previous_previous[1])
-        previous_previous = previous
-        previous = (record.POS, record.ALT, record.INFO['AF'])
-    return variants
+    for pos in variants:
+        highest_freq = 0
+        kept_var = None
+        for var in pos:
+            if variants[pos][var][0] > highest_freq:
+                highest_freq = variants[pos][var][0]
+                kept_var = var
+        highest_variants[pos] = str(kept_var)
 
+    for pos in highest_variants:
+        reference_sequence[pos - 1] = highest_variants[pos]
+    
+    return reference_sequence
+        
 
-
-
-def consensus(reference, variants):
-    """
-    Input:
-        variants: most common variant that replaces reference in consensus, in the form (position, variant)
-        reference: the reference sequence
-    Output:
-        reference: reference is updated to be the consensus
-    """
-    for variant in variants:
-        reference[(variant[0]) - 1] = variant[1]
-    return reference
