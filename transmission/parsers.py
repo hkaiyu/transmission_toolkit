7d4ad8c8b0dbb60d94d572ff0f62b0847ad9eaff
Kaiyu Hernandez <kaiyuhernandez@gmail.com>
2020-07-07 15:35:36 -0500

    change code organization

--- transmission/parsers.py
+++ transmission/parsers.py
@@ -1,4 +1,8 @@
 """Module containing classes and functions for parsing different types of data"""
+# Standard library import
+import os
+import glob
+import copy
 
 #Local import
 from transmission_toolkit.utils import Data, Biallelic, Multiallelic
@@ -6,133 +10,174 @@ from transmission_toolkit.utils import Data, Biallelic, Multiallelic
 #Third party import
 import vcf
 
-def _is_valid_lfv(min_read_depth, max_AF, var_reads, total_reads):
-    """
-    Boolean function called as a helper for determining if a variant fits the
-    user-defined parameters for parsing the data.
-    """
-    # Calculate allele frequency
-    freq = var_reads / total_reads
+class Parsers:
 
-    #If the allele passes restrictions, return True
-    if var_reads >= min_read_depth and freq < max_AF:
-        return True
+    def __init__(self, path_to_vcfs, ref_seq):
+        self.path = path_to_vcfs
+        self.ref = ref_seq
 
-    #Otherwise, return False
-    return False
+    def _is_valid_lfv(self, min_read_depth, max_AF, var_reads, total_reads):
+        """
+        Boolean function called as a helper for determining if a variant fits the
+        user-defined parameters for parsing the data.
+        """
+        # Calculate allele frequency
+        freq = var_reads / total_reads
 
-def extract_lfv(filename, min_read_depth=0, max_AF=1, parse_type="biallelic", store_reference=True, masks=None, mask_status='hide'):
-    """
-    Extracts variant data from VCF and creates a dictionary storing data
-    in the form: {position: {variant: [frequency, depth]}}.
-    """
+        #If the allele passes restrictions, return True
+        if var_reads >= min_read_depth and freq < max_AF:
+            return True
 
-    #### Handle Errors #####
-    PARSE_TYPES = {"biallelic", "multiallelic"}
-    MASK_TYPES = {"hide", "highlight"}
-    if min_read_depth < 0 or max_AF > 1 or parse_type not in PARSE_TYPES or str(mask_status) not in MASK_TYPES:
-        raise ValueError("Invalid input.")
+        #Otherwise, return False
+        return False
+    
+    def extract_lfv(self, filename, min_read_depth=0, max_AF=1, parse_type="biallelic", store_reference=True, masks=None, mask_status='hide'):
+        """
+        Extracts variant data from VCF and creates a dictionary storing data
+        in the form: {position: {variant: [frequency, depth]}}.
+        """
 
-    #########################
+        #### Handle Errors #####
+        PARSE_TYPES = {"biallelic", "multiallelic"}
+        MASK_TYPES = {"hide", "highlight"}
+        if min_read_depth < 0 or max_AF > 1 or parse_type not in PARSE_TYPES or str(mask_status) not in MASK_TYPES:
+            raise ValueError("Invalid input.")
 
-    #Parse mask file if mask file is inputted
-    mask_positions = []
-    if masks != None:
-        with open(masks, "r") as mask_file:
-            for line in mask_file:
-                comma_split = line.split(",")
-                for item in comma_split:
-                    nums = item.split("-")
-                    if len(nums) == 1:
-                        mask_positions.append(int(nums))
-                    else:
-                        for num in range(nums[0], nums[1]):
-                            mask_positions.append(int(num))
+        #########################
 
-    lfv_data = Biallelic() if parse_type == "biallelic" else Multiallelic()
-    data = vcf.Reader(open(filename, 'r'))
-    ref_data = {}
+        #Parse mask file if mask file is inputted
+        mask_positions = []
+        if masks != None:
+            with open(masks, "r") as mask_file:
+                for line in mask_file:
+                    comma_split = line.split(",")
+                    for item in comma_split:
+                        nums = item.split("-")
+                        if len(nums) == 1:
+                            mask_positions.append(int(nums))
+                        else:
+                            for num in range(nums[0], nums[1]):
+                                mask_positions.append(int(num))
 
-    for record in data:
+        lfv_data = Biallelic() if parse_type == "biallelic" else Multiallelic()
+        data = vcf.Reader(open(os.path.join(self.path, filename), 'r'))
+        ref_data = {}
 
-        # Calculate amount of reads support variant
-        var_depth = record.INFO['DP4'][2] + record.INFO['DP4'][3]
+        for record in data:
 
-        # Calculate amount of reads at particular location
-        raw_depth = record.INFO['DP']
+            # Calculate amount of reads support variant
+            var_depth = record.INFO['DP4'][2] + record.INFO['DP4'][3]
 
-        # Find data in VCF file
-        pos, var, freq = record.POS, str(record.ALT[0]), float(var_depth / raw_depth)
+            # Calculate amount of reads at particular location
+            raw_depth = record.INFO['DP']
 
-        #doesn't include masked positions based on user settings
-        if pos in mask_positions and mask_status == 'hide':
-            break
+            # Find data in VCF file
+            pos, var, freq = record.POS, str(record.ALT[0]), float(var_depth / raw_depth)
 
-        # If variant passes restrictions, store data
-        if _is_valid_lfv(min_read_depth, max_AF, var_depth, raw_depth):
-            lfv_data.store(pos, var, freq, var_depth)
+            #doesn't include masked positions based on user settings
+            if pos in mask_positions and mask_status == 'hide':
+                continue
 
-        if store_reference and not pos in ref_data:
-            # Calculate how many reads support reference
-            ref_depth = record.INFO['DP4'][0] + record.INFO['DP4'][1]
+            # If variant passes restrictions, store data
+            if self._is_valid_lfv(min_read_depth, max_AF, var_depth, raw_depth):
+                lfv_data.store(pos, var, freq, var_depth)
 
-            #Find reference allele
-            ref = str(record.REF[0])
-            
-            # If ref allele passes restrictions, store the data
-            if _is_valid_lfv(min_read_depth, max_AF, ref_depth, raw_depth):
-                ref_data[pos] = {ref: [(ref_depth / raw_depth), ref_depth]}
+            if store_reference and not pos in ref_data:
+                # Calculate how many reads support reference
+                ref_depth = record.INFO['DP4'][0] + record.INFO['DP4'][1]
 
-    # After parsing is complete, make object into a dictionary
-    lfv_data = dict(lfv_data)
+                #Find reference allele
+                ref = str(record.REF[0])
+                
+                # If ref allele passes restrictions, store the data
+                if self._is_valid_lfv(min_read_depth, max_AF, ref_depth, raw_depth):
+                    ref_data[pos] = {ref: [(ref_depth / raw_depth), ref_depth]}
 
-    # If we collected reference data, update lfv_data
-    if store_reference:
-        for pos in ref_data:
-            if not pos in lfv_data:
-                print("if")
-                lfv_data[pos] = ref_data[pos]
-            else:
-                print("else")
-                lfv_data[pos].update(ref_data[pos])
+        # After parsing is complete, make object into a dictionary
+        lfv_data = dict(lfv_data)
 
-    return lfv_data, mask_positions
+        # If we collected reference data, update lfv_data
+        if store_reference:
+            for pos in ref_data:
+                if not pos in lfv_data:
+                    print("if")
+                    lfv_data[pos] = ref_data[pos]
+                else:
+                    print("else")
+                    lfv_data[pos].update(ref_data[pos])
 
-def bb_input_data(donor, recip, min_read_depth=0, max_AF=1, parse_type="biallelic", store_reference=True, weighted=False):
-    """
-    Stores info from parsing VCF files to dictionary.
-    """
-    donor_data, donor_masks = extract_lfv(donor, min_read_depth=min_read_depth, max_AF=max_AF, parse_type=parse_type, store_reference=store_reference)
-    recip_data, recip_masks = extract_lfv(recip, min_read_depth=min_read_depth, max_AF=max_AF, parse_type=parse_type, store_reference=store_reference)
+        return lfv_data, mask_positions
 
-    shared_count = 0
+    def bb_input_data(self, donor, recip, min_read_depth=0, max_AF=1, parse_type="biallelic", store_reference=True, weighted=False):
+        """
+        Stores info from parsing VCF files to dictionary.
+        """
+        donor_data, donor_masks = self.extract_lfv(donor, min_read_depth=min_read_depth, max_AF=max_AF, parse_type=parse_type, store_reference=store_reference)
+        recip_data, recip_masks = self.extract_lfv(recip, min_read_depth=min_read_depth, max_AF=max_AF, parse_type=parse_type, store_reference=store_reference)
 
-    # Stored as {pos: {var: [donor freq., recip. freq]}} bc Maria had two bb input files 
-    # and one required all this info, might change later tho
-    bb_data = {} 
+        shared_count = 0
 
-    # Iterate through each variant at each position
-    for pos in donor_data: 
+        # Stored as {pos: {var: [donor freq., recip. freq]}} bc Maria had two bb input files 
+        # and one required all this info, might change later tho
+        bb_data = {} 
 
-        bb_data[pos] = {}
+        # Iterate through each variant at each position
+        for pos in donor_data: 
 
-        for var in donor_data[pos]:
+            bb_data[pos] = {}
 
-            # Store donor data
-            donor_freq = donor_data[pos][var][0]
-            bb_data[pos] = {var: [donor_freq, 0.0]}
-            if weighted == True:
-                donor_depth = donor_data[pos][var][1]
-                bb_data[pos] = {var: [donor_freq, 0.0, donor_depth, 0.0]}
+            for var in donor_data[pos]:
 
-            # If recipient has same variant at same location, store it
-            if pos in recip_data and var in recip_data[pos]:
-                recip_freq = recip_data[pos][var][0]
-                bb_data[pos][var][1] = recip_freq
-                shared_count += 1
+                # Store donor data
+                donor_freq = donor_data[pos][var][0]
+                bb_data[pos] = {var: [donor_freq, 0.0]}
                 if weighted == True:
-                    recip_depth = recip_data[pos][var][1]
-                    bb_data[pos][var][3] = recip_depth
+                    donor_depth = donor_data[pos][var][1]
+                    bb_data[pos] = {var: [donor_freq, 0.0, donor_depth, 0.0]}
+
+                # If recipient has same variant at same location, store it
+                if pos in recip_data and var in recip_data[pos]:
+                    recip_freq = recip_data[pos][var][0]
+                    bb_data[pos][var][1] = recip_freq
+                    shared_count += 1
+                    if weighted == True:
+                        recip_depth = recip_data[pos][var][1]
+                        bb_data[pos][var][3] = recip_depth
 
 
-    return (bb_data, shared_count, donor_masks, recip_masks)
+        return (bb_data, shared_count, donor_masks, recip_masks)
+
+    def consensus_sequence(self, vcf_file):
+        """
+        With a VCF file specified, builds consensus sequence.
+        """
+        consensus = copy.copy(self.ref)
+        variants = self.extract_lfv(vcf_file)
+        highest_variants = {}
+
+        for pos in variants:
+            highest_freq = 0
+            kept_var = None
+            for var in pos:
+                if variants[pos][var][0] > highest_freq:
+                    highest_freq = variants[pos][var][0]
+                    kept_var = var
+            highest_variants[pos] = str(kept_var)
+
+        for pos in highest_variants:
+            consensus[pos - 1] = highest_variants[pos]
+        
+        return consensus
+
+    def map_consensus(self):
+        """
+        Returns a dictionary mapping filenames to its respective consensus.
+
+        Args:
+            path (str): Path to folder containing vcf files
+            ref_seq (str): The reference genome, (maybe change to path to reference genome file)
+        """
+        consensus_dict = dict()
+        for vcf in glob.glob(os.path.join(self.path,"*.vcf")):
+            consensus_dict[vcf] = self.consensus_sequence(os.path.join(self.path, vcf))
+        return consensus_dict
--- transmission/writers.py
+++ transmission/writers.py
@@ -4,27 +4,51 @@
 import os
 import subprocess
 
-#Local import
+# Local import
 from transmission_toolkit import parsers
 
-def write_fasta(consensus_dict, output_file, line_length=80):
-	"""
-	Given a dictionary mapping id's to consensus sequences, returns 
-	fasta file with given information.
-	"""
-	with open(output_file, 'w') as f:
-		for id in consensus_dict:
-			f.write(f">{id}")
-			consensus = consensus_dict[id]
-			for i in range(0, len(consensus), line_length):
-				f.write(consensus[i: i+line_length])
+def write_fasta(consensus_dict, output_file, extension='fasta', output_dir= "", line_length=80):
+    """
+    Given a dictionary mapping patients to consensus sequences, returns 
+    fasta file with given information.
+    """
+    filepath = output_file + '.' + extension  
+
+    # If user specifies output directory
+    if output_dir:
+
+        # Check if directory already exists
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+        
+        # Update path to reflect newly added directory
+        filepath = os.path.join(output_dir, filepath)
+
+        # If file already exists in this location, remove it to overwrite it
+        if os.path.exists(filepath):
+            os.remove(filepath)
+
+    # Write file in specified path
+    with open(filepath, 'w') as f:
+        for fname in consensus_dict:
+            f.write(f">{fname}")
+            consensus = consensus_dict[fname]
+            for i in range(0, len(consensus), line_length):
+                f.write(consensus[i: i+line_length])
     
 def bb_file_writer(donor, recipient, parse_type="biallelic", min_read_depth=0, max_AF=1, var_calling_threshold=0.03):
     """
     Writes input file to BB_bottleneck software.
     """
     # Parse data and store outputs
-    vcf_data, shared_count = parsers.bb_input_data(donor, recipient, store_reference=False)[0], parsers.bb_input_data(donor, recipient, store_reference=False)[1]
+    parsed_data = parsers.bb_input_data(
+        donor,
+        recipient,
+        parse_type=parse_type,
+        min_read_depth=min_read_depth,
+        max_AF=max_AF)
+        
+    vcf_data, shared_count = parsed_data[0], parsed_data[1]
 
     # Create shortened filenames
     fname1 = donor.split("_")[0] 
@@ -37,3 +61,4 @@ def bb_file_writer(donor, recipient, parse_type="biallelic", min_read_depth=0, m
             for var in vcf_data[pos]:
                 if vcf_data[pos][var][0] > var_calling_threshold:
                     f.write(str(vcf_data[pos][var][0])+'\t'+str(vcf_data[pos][var][1])+'\n')
+
