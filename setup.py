1bdaff6bb0645adcac0c9b1063da0b4ac61a6ca6
Kaiyu Hernandez <kaiyuhernandez@gmail.com>
2020-07-08 21:18:09 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+DEPENDENCIES = ['']
+VERSION = '0.0'
+setup(
+    name="transmission_toolkit",
+    version="0.0",
+    packages=['transmission_toolkit'],
+    author="Kaiyu Hernandez and Mary Brady",
+    install_requires=DEPENDENCIES
+)
